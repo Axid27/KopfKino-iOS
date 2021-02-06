b80097efb3778f97a89bd98405d8b749915048f7
Pit kdw <kaldepitti@gmail.com>
2021-02-06 20:16:46 +0100

    Initial Commit

---
+++ KopfKinoApplication/KopfKinoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  KopfKinoApplicationApp.swift
+//  KopfKinoApplication
+//
+//  Created by Pit Kaldewey on 06.02.21.
+//
+
+import SwiftUI
+
+@main
+struct KopfKinoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
